f875a83b44cd572efd6780a2a5bd393b70b6c84d
thiago-igor <thiago.maia0606@gmail.com>
2023-01-05 10:43:39 -0300

    atualização

---
+++ curso em video/aula11Condicoes/ex008.js
@@ -0,0 +1,6 @@
+var vel = 60.5
+console.log(`A velocidade do seu carro é de ${vel} km/h`)
+if(vel > 60){
+    console.log(`Voce ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre com o sinto de segurança`)
