65623a1e7f95be7093d49d19ba25e784a7d7d9f5
thiago-igor <thiago.maia0606@gmail.com>
2023-01-18 16:52:12 -0300

    atualização

--- curso em video/aula14Ex/ex02/script.js
+++ curso em video/aula14Ex/ex02/script.js
@@ -1,12 +1,20 @@
 function verificar() {
     var num = window.document.querySelector('input#num')
-    var res = window.document.querySelector('div#res')
+    var tab = window.document.querySelector('select#tab')
 
-    num = Number(num.value)
+    
+    if(num.value.length == 0){ // verificando se esta vazio
+        window.alert('[Erro!] Por favor digite um numero')
+    }else{
 
-    for (let index = 1; index <= 10; index++) {
-        var mult = num * index
-        res += res.innerHTML =`${num} x ${index} = ${mult}`
-        
+        num = Number(num.value)
+    
+        tab.innerHTML =" "
+        for (let c = 1; c <= 10; c++) {
+            let item = document.createElement('option')// criando um objeto html no javaScript
+            item.text = `${num} x ${c} = ${num*c}`
+            tab.appendChild(item) // adiciona um elemento filho "item" dentro do "select"
+        }
     }
+    
 }
