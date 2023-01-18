e79cd75a01e62d84eaa766885415be8f577d347a
thiago-igor <thiago.maia0606@gmail.com>
2023-01-18 11:48:07 -0300

    atualização
    apenas pular uma linha

--- curso em video/aula14Ex/ex01/script.js
+++ curso em video/aula14Ex/ex01/script.js
@@ -7,7 +7,7 @@ function calcular() {
     if(ini.value.length == 0 || fim.value.length ==0 || passo.value.length == 0){
         window.alert('[ERRO] Faltam dados')
     }else{
-        res.innerHTML = `Contando:`
+        res.innerHTML = `Contando: <br>`
         let i = Number(ini.value)
         let f = Number(fim.value)
         let p = Number(passo.value)
