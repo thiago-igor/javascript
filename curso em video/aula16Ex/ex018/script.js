3424b8ebc82c42c927d5546fa8c97f1b6ffb4688
thiago-igor <thiago.maia0606@gmail.com>
2023-01-20 10:54:04 -0300

    Atualização

---
+++ curso em video/aula16Ex/ex018/script.js
@@ -0,0 +1,29 @@
+let num = window.document.querySelector('input#fnum')
+let lista = window.document.querySelector('select#flista')
+let res = window.document.querySelector('div#res')
+let valores = []
+
+function isNumero(n) {
+    if(Number(n) >= 1 && Number(n) <= 100){
+        return true
+    }else{
+        return false
+    }
+}
+
+function inLista(n, l) {
+    if(l.indexOf(Number(n)) != -1){
+        return true
+    }else{
+        return false
+    }
+}
+
+function adicionar() {
+    if(isNumero(num.value) && !inLista(num.value, valores)){
+        window.alert('tudo ok')
+    }
+    else{
+        window.alert('Valor invalido ou numero ja encontrado na lista')
+    }
+}
