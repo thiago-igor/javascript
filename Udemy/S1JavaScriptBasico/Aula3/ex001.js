5febc0d8c44acf53ce2189fac601bebe78fab671
thiago-igor <thiago.maia0606@gmail.com>
2023-10-09 09:49:44 -0300

    Atualização

---
+++ Udemy/S1JavaScriptBasico/Aula3/ex001.js
@@ -0,0 +1,2 @@
+console.log('Meu "NOME"', 'e estou aprendendo Java script as', 10 ,'hr da manhã')
+// Expressão separada por virgulas 
---
+++ Udemy/S1JavaScriptBasico/Aula6/index.js
@@ -0,0 +1,4 @@
+let nome = 'JOAO'
+
+console.log(`${nome} Nasceu em 1984`)
+console.log('Em 2008', nome, 'casou-se com maria')
