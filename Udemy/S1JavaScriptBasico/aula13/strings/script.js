d133e28f1a2a8411d885eea48deb68a24b83589f
thiago-igor <thiago.maia0606@gmail.com>
2023-10-10 21:58:13 -0300

    Atualização

---
+++ Udemy/S1JavaScriptBasico/aula13/strings/script.js
@@ -0,0 +1,8 @@
+// Algumas Regras de Strings:
+
+//               01234567
+let umaString = 'Um texto';
+console.log(umaString[4])  // pega o caracter da posição 4
+console.log(umaString.charAt(5)) // diz qual letra esta na posição 5
+console.log(umaString.indexOf('texto')) // qual indice começa a palavra texto (OBS: caso nao exista, retorna '-1')
+console.log(umaString.lastIndexOf('m', 3)) // começa de traz para frente, a partir do indice 3 
