2941c4b787b505393c5da4a2f489d244b02b2412
thiago-igor <thiago.maia0606@gmail.com>
2023-10-13 10:28:37 -0300

    Atualização

---
+++ Udemy/S1JavaScriptBasico/aula14/index.js
@@ -0,0 +1,26 @@
+let num1 = 10;
+let num2 = 2.5213;
+let num3 = 0.7;
+let num4 = 0.1;
+
+// transformando num1 em string para fazer uma concatenação
+console.log(num1.toString() + num2) 
+
+//Definir casas decimais:
+console.log(num2.toFixed(2))
+
+// Representação binaria de num:
+console.log(num1.toString(2))
+
+//Retornar se o number é inteiro:(Boolean)
+console.log(Number.isInteger(num1))
+
+//Resolver imprecisao do JS
+let soma = num3 + num4
+
+console.log(soma) // imprecisao
+
+soma = parseFloat(soma.toFixed(2)) // conta precisa 
+console.log(soma)
+
+
