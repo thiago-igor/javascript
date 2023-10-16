cd7d77059b419b5c44a4d5d43da5462e1f070d79
thiago-igor <thiago.maia0606@gmail.com>
2023-10-16 10:00:42 -0300

    Atualização

---
+++ Udemy/S1JavaScriptBasico/aula14/number/math.js
@@ -0,0 +1,20 @@
+let num1 = 9.54587
+
+let num2 = Math.floor(num1); // Arredonda para cima
+
+let num3 = Math.ceil(num1); // Arredonda para baixo
+
+let num4 = Math.round(num1); // arredonda para o mais prox
+
+console.log(Math.max(1,2,3,4,5,-10,-50,9,15,6)) // Pega o max da lista
+
+console.log(Math.min(1,2,3,4,5,-10,-50,9,15,6)) // Pega o min da lista
+
+console.log(Math.random()) // numero aleatorio entre 0 e 1 
+
+const aleatorio = Math.round(Math.random() * (10 - 5) +5 ); // Numero aleatorio entre 5 e 10
+console.log(aleatorio);
+
+let num5 = (16 ** 1/2) // raiz quadrada
+console.log(num5)
+
