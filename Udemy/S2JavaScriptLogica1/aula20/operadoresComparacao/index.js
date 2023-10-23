e9281689cb302133a72c6824aeef929a5c60c1a9
thiago-igor <thiago.maia0606@gmail.com>
2023-10-23 11:52:15 -0300

    Atualização
    Logica para computação

---
+++ Udemy/S2JavaScriptLogica1/aula20/operadoresComparacao/index.js
@@ -0,0 +1,13 @@
+/*
+== igualdade (Valor) [**Nao usamos]
+=== igualdade estrita (valor e tipo) [Usamos]
+
+!= diferente (Valor) [**Nao usamos]
+!== diferente (valor e tipo) [Usamos]
+*/
+
+const num1 = 10;
+const num2 = '10';
+const comp = num1 === num2 // verifica valor e tipo 
+console.log(comp)
+
---
+++ Udemy/S2JavaScriptLogica1/aula21/logica/index.js
@@ -0,0 +1,5 @@
+const usuario = 'Luiz'; // form usuario digitou
+const senha = '123456'; // form usuario digitou
+
+const vaiLogar = usuario === 'Luiz' && senha === '123456'
+console.log(vaiLogar)
