78fca2e1aba4f648fd8350d95f657a75d37f6451
thiago-igor <thiago.maia0606@gmail.com>
2023-10-30 11:35:54 -0300

    Atualização

---
+++ Udemy/S2JavaScriptLogica1/aula27/switch/index2.js
@@ -0,0 +1,34 @@
+function getDiaSemanaTexto(diaSemana) {
+    let diaSemanaTexto;
+    switch (diaSemana) {
+        case 0:
+            diaSemanaTexto = 'Domingo';
+            return diaSemanaTexto;
+        case 1:
+            diaSemanaTexto = 'Segunda';
+            return diaSemanaTexto;
+        case 2:
+            diaSemanaTexto = 'Terça';
+            return diaSemanaTexto;
+        case 3:
+            diaSemanaTexto = 'Quarta';
+            return diaSemanaTexto;
+        case 4:
+            diaSemanaTexto = 'Quinta';
+            return diaSemanaTexto;
+        case 5:
+            diaSemanaTexto = 'Sexta';
+            return diaSemanaTexto;
+        case 6:
+            diaSemanaTexto = 'Sabado';
+            return diaSemanaTexto;
+        default:
+            return diaSemanaTexto;;
+    }
+}
+
+const data = new Date();
+const diaSemana = data.getDay(); // argumento para função
+const diaSemanaTexto = getDiaSemanaTexto(diaSemana); // chamando função
+
+console.log(diaSemana, diaSemanaTexto)
