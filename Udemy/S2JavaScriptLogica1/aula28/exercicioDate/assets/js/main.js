a8de42b08766f0116ce03a4a7b30fa24824bfe30
thiago-igor <thiago.maia0606@gmail.com>
2023-10-31 21:24:09 -0300

    Atualização

--- Udemy/S2JavaScriptLogica1/aula28/exercicioDate/assets/js/main.js
+++ Udemy/S2JavaScriptLogica1/aula28/exercicioDate/assets/js/main.js
@@ -18,14 +18,64 @@ function diaAtual(dat) {
         case 2:
             dat.innerHTML = `Terça-Feira, ${dia} de`
             break;
-    
+        case 3:
+            dat.innerHTML = `Quarta-Feira, ${dia} de`
+            break;
+        case 4:
+            dat.innerHTML = `Quinta-Feira, ${dia} de`
+            break;
+        case 5:
+            dat.innerHTML = `Sexta-Feira, ${dia} de`
+            break;
+        case 6:
+            dat.innerHTML = `Sábado, ${dia} de`
+            break;
         default:
             break;
     }
 
     switch (mes) {
+        case 0:
+            dat.innerHTML += ` de ${ano}  ${hora}:${min}`
+            break;
+        case 0:
+            dat.innerHTML += ` de ${ano}  ${hora}:${min}`
+            break;
+        case 1:
+            dat.innerHTML += ` de ${ano}  ${hora}:${min}`
+            break;
+        case 2:
+            dat.innerHTML += ` de ${ano}  ${hora}:${min}`
+            break;
+            case 0:
+            dat.innerHTML += ` de ${ano}  ${hora}:${min}`
+                break;
+            case 3:
+            dat.innerHTML += ` de ${ano}  ${hora}:${min}`
+            break;
+        case 4:
+            dat.innerHTML += ` de ${ano}  ${hora}:${min}`
+            break;
+        case 5:
+            dat.innerHTML += ` de ${ano}  ${hora}:${min}`
+            break;
+        case 6:
+            dat.innerHTML += ` de ${ano}  ${hora}:${min}`
+            break;
+        case 7:
+            dat.innerHTML += ` de ${ano}  ${hora}:${min}`
+            break;
+        case 8:
+            dat.innerHTML += ` de ${ano}  ${hora}:${min}`
+            break;
         case 9:
             dat.innerHTML += `Outubro de ${ano}  ${hora}:${min}`
+            break;
+        case 10:
+            dat.innerHTML += ` de ${ano}  ${hora}:${min}`
+            break;
+        case 11:
+            dat.innerHTML += ` de ${ano}  ${hora}:${min}`
             break;
     
         default:
