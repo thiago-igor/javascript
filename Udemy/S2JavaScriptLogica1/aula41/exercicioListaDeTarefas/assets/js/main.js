bde90f3ab793578ddd6fe73e77959f92676e8895
thiago-igor <thiago.maia0606@gmail.com>
2023-11-08 09:36:17 -0300

    Atualização

--- Udemy/S2JavaScriptLogica1/aula41/exercicioListaDeTarefas/assets/js/main.js
+++ Udemy/S2JavaScriptLogica1/aula41/exercicioListaDeTarefas/assets/js/main.js
@@ -19,8 +19,14 @@ function limparInput() {
     inputTarefa.focus(); // volta para o lable depois de apertar enter
 }
 
-function criaBotaoApagar(li) {
-    
+function criaBotaoApagar(li) { // criando botao apagar ao lado da lista li
+    li.innerText += ' ';
+    const botaoApagar = document.createElement('button');
+    botaoApagar.innerText = 'Apagar'
+    // botaoApagar.classList.add('apagar') // cria classe
+    botaoApagar.setAttribute('class', 'apagar'); // outro jeito de criar classe para o botao
+    botaoApagar.setAttribute('title', 'Apagar esta tarefa'); // texto criado quando passa o mpuse em cima do botao apagar
+    li.appendChild(botaoApagar)
 }
 
 function criaTarefa(textoInput) {
@@ -28,9 +34,45 @@ function criaTarefa(textoInput) {
     li.innerText = textoInput
     tarefas.appendChild(li);
     limparInput();
+    criaBotaoApagar(li);
+    salvarTarefas();
 }
 
 btnTarefa.addEventListener('click', function(){
     if(!inputTarefa.value) return; // se inputTarefa === ''
     criaTarefa(inputTarefa.value)
 })
+
+document.addEventListener('click', function(e){
+    const el = e.target; // captura onde esta sendo clicado com o mouse 
+    if(el.classList.contains('apagar')){ // se esse botao contem a classe 'apagar'
+    el.parentElement.remove(); // // Apagando o pai do botao 'apagar':
+    salvarTarefas();
+    }
+})
+
+function salvarTarefas(){
+    const liTarefas = tarefas.querySelectorAll('li')// seleciona todos 'li' de da Ul 'tarefas'
+    const listaDeTarefas = [];
+
+    for(let tarefa of liTarefas){
+        let tarefaTexto = tarefa.innerText; // colocando as tarefas em um array
+        tarefaTexto = tarefaTexto
+        .replace('Apagar', '').trim(); // substitui 'Apagar' por '' e trim() tira espaços np final da string 
+        listaDeTarefas.push(tarefaTexto);
+    }
+    const tarefasJSON = JSON.stringify(listaDeTarefas) // transformando array em String
+    localStorage.setItem('tarefas', tarefasJSON); // Salvar no navegador as tarefas
+}
+
+function adicionaTarefasSalvas() {
+    const tarefas = localStorage.getItem('tarefas');
+    const listaDeTarefas = JSON.parse(tarefas) // volta a ser um array
+    console.log(listaDeTarefas);
+
+    for(let tarefa of listaDeTarefas){
+        criaTarefa(tarefa);
+    }
+}
+
+adicionaTarefasSalvas();
