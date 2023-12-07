0dd738f2c17ad90873fc02eed07d64ba693d05b9
thiago-igor <thiago.maia0606@gmail.com>
2023-12-07 11:12:32 -0300

    Atualização
    Aprendendo sobre ForEach( )

---
+++ Udemy/S4Arrays/aula61/forEach/index.js
@@ -0,0 +1,6 @@
+// ForEach para iterar dentro do array
+
+const a1 = [10, 20, 30, 40, 50, 60, 70]
+a1.forEach(function (valor, indice, array) { // faz um for e me retorna os valores de 'valor' 'indice' 'array'
+    console.log(valor, indice) 
+})
